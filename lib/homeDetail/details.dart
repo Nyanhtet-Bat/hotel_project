1da71a7b4a10795d372d74193bf8abe547a03f55
Nyan Lin Htet <57280584+Nyanhtet-Bat@users.noreply.github.com>
2020-02-19 23:36:26 +0630

    'add'

--- lib/homeDetail/details.dart
+++ lib/homeDetail/details.dart
@@ -3,68 +3,64 @@ import 'package:project_trip_app/booking_calendar/date_range_picker.dart';
 import 'package:project_trip_app/model/hotel_model.dart';
 import 'package:project_trip_app/model/room_types.dart';
 
-
 class HotelDetails extends StatelessWidget {
-
   final RoomTypes roomTypes;
 
   HotelDetails(this.roomTypes);
-  bodyWidget(BuildContext context) =>
-      Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.black,
-        ),
-        child: Stack(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topLeft,
-                child: AppBar(
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
-                ),
+
+  bodyWidget(BuildContext context) => Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+          ),
+          child: Stack(children: <Widget>[
+            Align(
+              alignment: Alignment.topLeft,
+              child: AppBar(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
               ),
-              Image.network(roomTypes.image, fit: BoxFit.cover),
-              SingleChildScrollView(
-                padding: const EdgeInsets.only(top: 200),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      roomTypes.roomName,
-                      style: TextStyle(color: Colors.white, fontSize: 28.0),
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 8.0, horizontal: 16.0),
-                          decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(20.0)),
-                          child: Text(
-                            "8.4/85 reviews",
-                            style: TextStyle(color: Colors.white),
-                          ),
+            ),
+            Image.network(roomTypes.image, fit: BoxFit.cover),
+            SingleChildScrollView(
+              padding: const EdgeInsets.only(top: 200),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    roomTypes.roomName,
+                    style: TextStyle(color: Colors.white, fontSize: 28.0),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 16.0),
+                        decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(20.0)),
+                        child: Text(
+                          "8.4/10 reviews",
+                          style: TextStyle(color: Colors.white),
                         ),
-                        Spacer(),
-                        IconButton(
-                          color: Colors.white,
-                          icon: Icon(Icons.favorite_border),
-                          onPressed: () {
-                          },
-                        )
-                      ],
-                    ),
-                    Container(
-                        padding: const EdgeInsets.all(16.0),
-                        color: Colors.black,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            Row(
-                              children: <Widget>[
+                      ),
+                      Spacer(),
+                      IconButton(
+                        color: Colors.white,
+                        icon: Icon(Icons.favorite_border),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                  Container(
+                      padding: const EdgeInsets.all(16.0),
+                      color: Colors.black,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
 //                                Expanded(
 //                                  child: Column(
 //                                    children: <Widget>[
@@ -95,52 +91,75 @@ class HotelDetails extends StatelessWidget {
 //                                    ],
 //                                  ),
 //                                ),
-                                Column(
-                                  children: <Widget>[
-                                    Text("\$ 200"),
-                                    Text("/per night")
-                                  ],
-                                )
-                              ],
+                              Column(
+                                children: <Widget>[
+                                  Text(
+                                    "${roomTypes.price}",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  Text(
+                                    "/per night",
+                                    style: TextStyle(color: Colors.white),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 20.0),
+                          FittedBox(
+                            fit: BoxFit.fitWidth,
+                            child: RaisedButton(
+                              color: Colors.purple,
+                              textColor: Colors.white,
+                              child: Text("Book Now"),
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 16.0, horizontal: 32.0),
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => HotelDateView(
+                                              hotel: Hotel(
+                                                  "", null, null, null, ""),
+                                            )));
+                              },
                             ),
-                            const SizedBox(height: 20.0),
-                            FittedBox(
-                              fit: BoxFit.fitWidth,
-                              child: RaisedButton(
-                                color: Colors.purple,
-                                textColor: Colors.white,
-                                child: Text("Book Now"),
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 16.0, horizontal: 32.0),
-                                onPressed: (){
-                                  Navigator.push(context, MaterialPageRoute(
-                                      builder: (context) =>
-                                          HotelDateView(hotel: Hotel("", null, null,null,""),)
-                                  ));
-
-                                },
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 20.0,
-                            ),
-                            Text("Description".toUpperCase(),style: TextStyle(color: Colors.white)),
-                            Text("askldjflkjasdkfja lsdjfoajsdlkfja"
-                                "asdgjfasdkjflkjasdlkjflkasdjfas"
-                                "adsfjhklasdjflkjasd;lkjfasas"
-                                "ksadjflkjasdlkfjlkasjfd",
-                                style: TextStyle(color: Colors.white))
-
-                          ],
-
-                        )
-                    )
-
-                  ],
-                ),
+                          ),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          Text("Description".toUpperCase(),
+                              style: TextStyle(color: Colors.white)),
+                          Text("${roomTypes.description}",
+                              style: TextStyle(color: Colors.white)),
+                          Text(""),
+                          Text("Facility".toUpperCase(),
+                              style: TextStyle(color: Colors.white)),
+                          Text("${roomTypes.facilityName}",
+                              style: TextStyle(color: Colors.white)),
+                          Text(""),
+                          Text("Total Room".toUpperCase(),
+                              style: TextStyle(color: Colors.white)),
+                          Text("${roomTypes.totalRoom}",
+                              style: TextStyle(color: Colors.white)),
+                          Text(""),
+                          Text("Opened @".toUpperCase(),
+                              style: TextStyle(color: Colors.white)),
+                          Text("${roomTypes.createdAt}",
+                              style: TextStyle(color: Colors.white)),
+                          Text(""),
+                          Text("Update".toUpperCase(),
+                              style: TextStyle(color: Colors.white)),
+                          Text("${roomTypes.updatedAt}",
+                              style: TextStyle(color: Colors.white)),
+                          Text(""),
+                        ],
+                      ))
+                ],
               ),
-              Align(
-                alignment: Alignment.bottomLeft,
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
 //                child: BottomNavigationBar(
 //                  backgroundColor: Colors.black,
 //                  items: [
@@ -152,15 +171,16 @@ class HotelDetails extends StatelessWidget {
 //                        icon: Icon(Icons.settings, color: Colors.white), title: Text("Settings",style: TextStyle(color: Colors.white),)),
 //                  ],
 //                ),
-              ),
-            ]
+            ),
+          ]),
         ),
-      ),
-    );
+      );
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return Scaffold(
+      body: bodyWidget(context),
+    );
   }
-  }
+}
